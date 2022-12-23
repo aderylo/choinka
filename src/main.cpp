3b7ddb77f9b8729c6e10e28a13fe4d611bb3a58a
aderylo <a.m.derylo@gmail.com>
2022-12-23 10:52:15 +0100

    init
    Signed-off-by: aderylo <a.m.derylo@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+
+
+   return 0;
+}
