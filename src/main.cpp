3fb83ac69f06a062b64cdbbd75ab8541c11f6077
aderylo <a.m.derylo@gmail.com>
2022-12-23 11:16:02 +0100

    Read input
    Signed-off-by: aderylo <a.m.derylo@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 
 using namespace std;
@@ -9,6 +10,41 @@ int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(NULL);
 
+  char orderType;
+  size_t n, q, v, x;
+  vector<size_t> christmastTree, colours;
 
-   return 0;
+  cin >> n >> q;
+
+  for (size_t i = 0; i < n - 1; i++) {
+    christmastTree.push_back(0);
+    cin >> christmastTree.at(i);
+  }
+
+  for (size_t i = 0; i < n; i++) {
+    colours.push_back(0);
+    cin >> colours.at(i);
+  }
+
+  // preproccessing
+
+
+  // handling questions
+
+  for (size_t i = 0; i < q; i++) {
+    cin >> orderType;
+
+    if (orderType == '?') {
+      cin >> v;
+      // hanlde question;
+    }
+
+    if (orderType == 'z') {
+      cin >> v >> x;
+      // hanlde color change
+    }
+  }
+
+
+  return 0;
 }
