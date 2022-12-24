246fa51edccc053490ad26988a68dcda89d09e49
aderylo <a.m.derylo@gmail.com>
2022-12-24 14:32:43 +0100

    Implement color update in segment tree
    Signed-off-by: aderylo <a.m.derylo@gmail.com>

--- src/segmentTree.hh
+++ src/segmentTree.hh
@@ -23,17 +23,20 @@ class SegmentTreeNode {
 
 class SegmentTree {
  public:
-  explicit SegmentTree(vector<size_t> leafs);
+  explicit SegmentTree(vector<size_t> elems);
   explicit SegmentTree(){};
 
   bool almostHomogenousSegment(size_t start, size_t end);
 
-  void setValue(size_t leaf, size_t value);
+  void setValue(size_t elemIdx, size_t value);
 
  private:
+  void updateValue(size_t v, size_t elemIdx, size_t tl, size_t tr, size_t newValue,
+                   size_t oldValue);
   void build(vector<size_t> a, size_t v, size_t tl, size_t tr);
 
   vector<SegmentTreeNode> tree;
+  vector<size_t> leafs;
 };
 
 void SegmentTree::build(vector<size_t> a, size_t v, size_t tl, size_t tr) {
@@ -52,10 +55,30 @@ void SegmentTree::build(vector<size_t> a, size_t v, size_t tl, size_t tr) {
   }
 }
 
-
-SegmentTree::SegmentTree(vector<size_t> leafs) {
-  tree = vector<SegmentTreeNode>(MULTIPLE * leafs.size());
-  build(leafs, 1, 0, leafs.size() - 1);
+SegmentTree::SegmentTree(vector<size_t> elems) {
+  tree = vector<SegmentTreeNode>(MULTIPLE * elems.size());
+  build(elems, 1, 0, elems.size() - 1);
+  leafs = elems;
 }
+
+void SegmentTree::updateValue(size_t v, size_t elemIdx, size_t tl, size_t tr, size_t newValue,
+                              size_t oldValue) {
+  tree.at(v).values.erase(oldValue);
+  tree.at(v).values.insert(newValue);
+
+  if (tl != tr) {
+    size_t tm = (tl + tr) / 2;
+    if (elemIdx <= tm)
+      updateValue(v * 2, elemIdx, tl, tr, newValue, oldValue);
+    else
+      updateValue(v * 2 + 1, elemIdx, tm + 1, tr, newValue, oldValue);
+  }
+}
+
+void SegmentTree::setValue(size_t elemIdx, size_t value) {
+  updateValue(1, elemIdx, 0, leafs.size() - 1, value, leafs.at(elemIdx));
+}
+
+// bool SegmentTree::almostHomogenousSegment(size_t start, size_t end) {}
 
 #endif  // CHOINKA_SEGMENT_TREE
